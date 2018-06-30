f05e8fd14a28c9da5e988f44b9bc152e1215b6bf
Elijah Smith <elijahs2106@gmail.com>
2018-06-30 19:40:28 -0400

    Add buttons to return to moves in game history

--- src/index.js
+++ src/index.js
@@ -78,6 +78,19 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                `Go to move #${move}` :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        });
+
         const status = winner ? `Winner: ${winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
         return (
@@ -90,7 +103,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
       );
